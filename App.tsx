991a1d997411c7b629997abb6e618fd84329d2f8
sharanpreet singh <sharanpreet1831.be21@chitkara.edu.in>
2025-04-24 11:58:12 +0530

    Setup Commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import {  SafeAreaView, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
+
